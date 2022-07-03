3239751e1016c8223a2c96985ebc5c7a7d420966
vayamos-exchange <admin@vayamos.cc>
2022-07-03 23:38:33 +0200

    Remove return from IERC

--- IERC20.sol
+++ IERC20.sol
@@ -2,6 +2,6 @@
 pragma solidity ^0.8;
 
 interface IERC20 {
-    function transfer(address to, uint256 amount) external returns (bool);
+    function transfer(address to, uint256 amount) external;
     function balanceOf(address account) external view returns (uint256);
 }
